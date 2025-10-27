8dc7dd54dd29212d078d95da6ece6559c60e84c7
Bian-Mu <2570772184@qq.com>
2025-10-27 16:22:10 +0800

    fix:修改了样式

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,87 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from 'react';
+import {
+  MenuFoldOutlined,
+  MenuUnfoldOutlined,
+  ShoppingOutlined,
+  UserOutlined,
+} from '@ant-design/icons';
+import { Button, ConfigProvider, Layout, Menu, theme } from 'antd';
 
-function App() {
-  const [count, setCount] = useState(0)
+const { Header, Sider, Content } = Layout;
 
+const App: React.FC = () => {
+
+  const [collapsed, setCollapsed] = useState(false);
+  const [isDark, setIsDark] = useState(false)
+  const lightTheme = {
+    token: {
+      colorPrimary: '#1890ff',
+      bodyBg: '#f5f5f5',
+      colorBgContainer: '#ffffff',
+      siderBg: '#ffffff'
+    },
+  };
+
+  const darkTheme = {
+    token: {
+      colorPrimary: '#177ddc',
+      bodyBg: '#000000',
+      colorBgContainer: '#141414',
+      colorText: 'rgba(255, 255, 255, 0.85)',
+      siderBg: '#000000'
+    },
+  };
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <ConfigProvider theme={isDark ? darkTheme : lightTheme}>
+      <Layout className='h-full'>
 
-export default App
+        <Sider style={{ background: 'siderBg' }} trigger={null} collapsible collapsed={collapsed}>
+          <div className="demo-logo-vertical" />
+          <p className='text-2xl '>Online-Shopping</p>
+          <Menu
+            mode="inline"
+            defaultSelectedKeys={['2']}
+            items={[
+              {
+                key: '1',
+                icon: <UserOutlined />,
+                label: '我的',
+              },
+              {
+                key: '2',
+                icon: <ShoppingOutlined />,
+                label: '商品',
+              }
+            ]}
+          />
+        </Sider>
+        <Layout>
+          <Header style={{ padding: 0 }}>
+            <Button
+              type="text"
+              icon={collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
+              onClick={() => setCollapsed(!collapsed)}
+              style={{
+                fontSize: '16px',
+                width: 64,
+                height: 64,
+              }}
+            />
+          </Header>
+          <Content
+            style={{
+              margin: '24px 16px',
+              padding: 24,
+              minHeight: 280
+            }}
+          >
+            Content
+          </Content>
+        </Layout>
+      </Layout>
+    </ConfigProvider>
+
+  );
+};
+
+export default App;

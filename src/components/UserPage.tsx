40569912036eac0e0cf212139f0ff9941c2e1538
Bian-Mu <2570772184@qq.com>
2025-11-03 16:17:58 +0800

    feat:login logic

--- src/components/UserPage.tsx
+++ src/components/UserPage.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import type { User } from '../DataProps';
-import fakeUsers from "../fakeData/users.json"
+import Login from './Login';
 
 const UserPage: React.FC = () => {
     const [user, setUser] = useState<User | null>(null);
@@ -15,37 +15,27 @@ const UserPage: React.FC = () => {
     }, []);
 
 
-    const merchant = fakeUsers[0]
-    const customer = fakeUsers[1]
+
 
     if (!user) {
         return (
-            <div>
-                <h3>请登录</h3>
-                <button onClick={() => {
-
-                    localStorage.setItem('userData', JSON.stringify(merchant));
-                    setUser(merchant);
-                    console.log('用户登录数据:', merchant);
-                }}>
-                    模拟登录
-                </button>
-            </div>
+            <Login setUser={setUser} />
         );
     }
 
-    return (
-        <div>
-            <h3>用户信息</h3>
-            <pre>{JSON.stringify(user, null, 2)}</pre>
-            <button onClick={() => {
-                localStorage.removeItem('userData');
-                setUser(null);
-            }}>
-                退出登录
-            </button>
-        </div>
-    );
+    if (!user.role) {
+        return (
+            <div>
+                商家
+            </div>
+        )
+    } else {
+        return (
+            <div>
+                顾客
+            </div>
+        )
+    }
 };
 
 export default UserPage;
